7bde4b4a75c3fc405eed29ed012530c3e866f127
Robert <robharmony@hotmail.com>
2024-06-09 10:09:45 -0400

    added columns

--- src/TopTenList/TopTenList.tsx
+++ src/TopTenList/TopTenList.tsx
@@ -4,6 +4,8 @@ import { AnimeInfoType } from "../apiFunctions";
 
 export const TopTenList = () => {
   const [allAnime, setAllAnime] = useState<AnimeInfoType[]>([]);
+  const [favoriteAnime, setFavoriteAnime] = useState<AnimeInfoType[]>([]);
+  const [topTenAnime, setTopTenAnime] = useState<AnimeInfoType[]>([]);
 
   useEffect(() => {
     const fetchAnime = async () => {
@@ -16,22 +18,58 @@ export const TopTenList = () => {
 
   return (
     <div id="animeList">
-      <section id="allAnime">
-        {allAnime.map((anime, index) => (
-          <div id={`card${index}`} key={index} className="card">
-            <h3 className="title">Title: {anime.title}</h3>
-            <h3 className="mediaType">Media: {anime.mediaType}</h3>
-            <div className="pictureContainer">
-              <img src={anime.pictureUrl} alt="pictureName" />
+      <div className="sectionContainer">
+        <h1>Anime List</h1>
+        <section className="listContainer" id="allAnime">
+          {allAnime.map((anime, index) => (
+            <div id={`card${index}`} key={index} className="card">
+              <h3 className="title">Title: {anime.title}</h3>
+              <h3 className="mediaType">Media: {anime.mediaType}</h3>
+              <div className="pictureContainer">
+                <img src={anime.pictureUrl} alt="pictureName" />
+              </div>
+              <div className="buttonContainer">
+                <button> Add to Favorites</button>
+              </div>
             </div>
-            <div className="buttonContainer">
-              <button> Add to Favorites</button>
+          ))}
+        </section>
+      </div>
+      <div className="sectionContainer">
+        <h1>Favorites</h1>
+        <section className="listContainer" id="favoriteAnime">
+          {allAnime.map((anime, index) => (
+            <div id={`card${index}`} key={index} className="card">
+              <h3 className="title">Title: {anime.title}</h3>
+              <h3 className="mediaType">Media: {anime.mediaType}</h3>
+              <div className="pictureContainer">
+                <img src={anime.pictureUrl} alt="pictureName" />
+              </div>
+              <div className="buttonContainer">
+                <button> Add to Favorites</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </section>
-      <section id="favoriteAnime"></section>
-      <section id="topTenAnime"></section>
+          ))}
+        </section>
+      </div>
+      <div className="sectionContainer">
+        <h1>Top Ten Anime</h1>
+
+        <section className="listContainer" id="topTenAnime">
+          {allAnime.map((anime, index) => (
+            <div id={`card${index}`} key={index} className="card">
+              <h3 className="title">Title: {anime.title}</h3>
+              <h3 className="mediaType">Media: {anime.mediaType}</h3>
+              <div className="pictureContainer">
+                <img src={anime.pictureUrl} alt="pictureName" />
+              </div>
+              <div className="buttonContainer">
+                <button> Remove from Top Ten</button>
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
     </div>
   );
 };
